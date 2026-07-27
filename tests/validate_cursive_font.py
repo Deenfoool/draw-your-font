9766d1e688b0cd2f74a4ef42d003118e3ed90ef5
Денис Маняхин <36488826+Deenfoool@users.noreply.github.com>
2026-07-27 20:32:27 +0300

    Validate descender outlines and vertical metrics with FontTools

--- tests/validate_cursive_font.py
+++ tests/validate_cursive_font.py
@@ -19,15 +19,9 @@ missing = sorted(required.difference(font.keys()))
 if missing:
     raise RuntimeError(f'В связном шрифте отсутствуют таблицы: {missing}')
 
-gsub_feature_records = font['GSUB'].table.FeatureList.FeatureRecord
-gsub_features = [record.FeatureTag for record in gsub_feature_records]
+gsub_features = [record.FeatureTag for record in font['GSUB'].table.FeatureList.FeatureRecord]
 if gsub_features != ['calt', 'rlig']:
     raise RuntimeError(f'Неожиданные GSUB-функции: {gsub_features}')
-
-feature_lookups = {record.FeatureTag: list(record.Feature.LookupListIndex) for record in gsub_feature_records}
-expected_feature_lookups = {'calt': [1, 3, 5], 'rlig': [1, 3, 5]}
-if feature_lookups != expected_feature_lookups:
-    raise RuntimeError(f'GSUB-функции ссылаются не на контекстные lookup: {feature_lookups}')
 
 gsub_scripts = [record.ScriptTag for record in font['GSUB'].table.ScriptList.ScriptRecord]
 if gsub_scripts != ['DFLT', 'cyrl']:
@@ -36,6 +30,13 @@ if gsub_scripts != ['DFLT', 'cyrl']:
 gsub_lookups = [lookup.LookupType for lookup in font['GSUB'].table.LookupList.Lookup]
 if gsub_lookups != [1, 6, 1, 6, 1, 6]:
     raise RuntimeError(f'Неверная последовательность GSUB lookup: {gsub_lookups}')
+
+feature_lookup_indices = {
+    record.FeatureTag: list(record.Feature.LookupListIndex)
+    for record in font['GSUB'].table.FeatureList.FeatureRecord
+}
+if feature_lookup_indices != {'calt': [1, 3, 5], 'rlig': [1, 3, 5]}:
+    raise RuntimeError(f'GSUB-функции ссылаются не только на контекстные lookup: {feature_lookup_indices}')
 
 gpos_features = [record.FeatureTag for record in font['GPOS'].table.FeatureList.FeatureRecord]
 if gpos_features != ['curs']:
@@ -56,6 +57,25 @@ entry_count = sum(record.EntryAnchor is not None for record in cursive_subtable.
 exit_count = sum(record.ExitAnchor is not None for record in cursive_subtable.EntryExitRecord)
 if not entry_count or not exit_count:
     raise RuntimeError('В GPOS отсутствуют входные или выходные курсивные якоря')
+
+glyph_order = font.getGlyphOrder()
+glyf = font['glyf']
+r_name = glyph_order[layout['forms']['р']['isol']]
+a_name = glyph_order[layout['forms']['а']['isol']]
+r_y_min = glyf[r_name].yMin
+a_y_min = glyf[a_name].yMin
+if r_y_min >= -80:
+    raise RuntimeError(f'Нижний элемент «р» не опущен ниже строки: yMin={r_y_min}')
+if r_y_min >= a_y_min - 40:
+    raise RuntimeError(f'«р» должна быть заметно ниже «а»: р={r_y_min}, а={a_y_min}')
+
+metrics = layout['metrics']
+if font['hhea'].descent > metrics['inkBottom']:
+    raise RuntimeError(f'hhea.descent не покрывает нижние элементы: {font["hhea"].descent} > {metrics["inkBottom"]}')
+if font['OS/2'].sTypoDescender > metrics['inkBottom']:
+    raise RuntimeError(f'OS/2 descender не покрывает нижние элементы: {font["OS/2"].sTypoDescender} > {metrics["inkBottom"]}')
+if metrics['descent'] > metrics['inkBottom'] - 20:
+    raise RuntimeError(f'В метриках нет безопасного нижнего поля: {metrics}')
 
 font_bytes = FONT_PATH.read_bytes()
 face = hb.Face(font_bytes)
@@ -84,6 +104,17 @@ actual_mama = shape('мама')
 if actual_mama != expected_mama:
     raise RuntimeError(f'HarfBuzz сформировал «мама» неверно: {actual_mama}, ожидалось {expected_mama}')
 
+expected_drozh = [
+    layout['forms']['д']['init'],
+    layout['forms']['р']['medi'],
+    layout['forms']['о']['medi'],
+    layout['forms']['ж']['medi'],
+    layout['forms']['ь']['fina'],
+]
+actual_drozh = shape('дрожь')
+if actual_drozh != expected_drozh:
+    raise RuntimeError(f'HarfBuzz сформировал «дрожь» неверно: {actual_drozh}, ожидалось {expected_drozh}')
+
 expected_single = [layout['forms']['а']['isol']]
 actual_single = shape('а')
 if actual_single != expected_single:
@@ -100,7 +131,8 @@ actual_words = shape('ма ма')
 if actual_words != expected_words:
     raise RuntimeError(f'Пробел не разорвал соединение: {actual_words}, ожидалось {expected_words}')
 
-print('Cursive FontTools and HarfBuzz validation: PASS')
-print('GSUB features:', gsub_features, feature_lookups)
+print('Cursive FontTools, HarfBuzz and descender validation: PASS')
+print('GSUB features:', gsub_features, feature_lookup_indices)
 print('GPOS feature:', gpos_features[0], f'anchors entry={entry_count} exit={exit_count}')
-print('Shaped мама:', actual_mama)
+print('Descenders:', {'р': r_y_min, 'а': a_y_min, 'hhea': font['hhea'].descent, 'OS/2': font['OS/2'].sTypoDescender})
+print('Shaped дрожь:', actual_drozh)
